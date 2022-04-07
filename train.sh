350f311b639bc34f200437f4064a43762161d1b7
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2022-04-07 11:26:19 +0200

    opdateringer (graph virker igen)

--- train.sh
+++ train.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
-#BSUB -J pilot
-#BSUB -o pilot_%J.out
-#BSUB -e pilot_%J.err
-#BSUB -q hpc
+#BSUB -J pilot_
+#BSUB -o pilot__%J.out
+#BSUB -e pilot__%J.err
+#BSUB -q gpuv100
+#BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 1
-#BSUB -R "rusage[mem=32G]"
+#BSUB -R "rusage[mem=128G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 24:00
+#BSUB -W 00:10
 #BSUB -B
 #BSUB -N
 
-source vqa_cuda/bin/activate
+nvidia-smi
+module load cuda/11.1
+/appl/cuda/11.1/samples/NVIDIA_CUDA-11.1_Samples/bin/x86_64/linux/release/deviceQuery
+
+source vqa2/bin/activate
 cd mmf
 
-module load cuda/10.2
-/appl/cuda/10.2/samples/NVIDIA_CUDA-10.2_Samples/bin/x86_64/linux/release/deviceQuery
-
-mmf_run config='configs/experiments/pilot/test.yaml' \
+mmf_run config='configs/experiments/pilot/grids.yaml' \
     datasets=okvqa \
     model=pilot \
     run_type=train_val \
     env.data_dir=/work3/s194262/torch/mmf/data \
-    env.save_dir=/work3/s194262/save/models/pilot_grids \
+    env.save_dir=/work3/s194262/save/models/pilot_grids2 \
     trainer.params.gpus=1 \
