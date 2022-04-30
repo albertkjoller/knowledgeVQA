02be18a0d1a26c686b71a61d5564a1c91665ee08
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2022-04-30 10:58:56 +0200

    debug

--- mmf/mmf/modules/metrics.py
+++ mmf/mmf/modules/metrics.py
@@ -268,7 +268,7 @@ class NumberbatchScore(BaseMetric):
         self.answer_vocab = self.answer_processor.answer_vocab
         # todo: update for new tokenizer in numberbatch
         #self.embedded_answer_vocab = self.graph_encoder({'tokens': [tokenize(sentence) for sentence in self.answer_vocab.word_list]})  # [batch_size, g_dim]
-        self.top_k = int(self.config.model_config[self.model].classifier.params.top_k)
+        self.top_k = int(self.config.model_config[self.config.model].classifier.params.top_k)
         #self.num_not_top_k = len(self.embedded_answer_vocab) - self.top_k # if classifier outputs embeddings
 
 
