77d06db75cdfd46372ec778c0fde3f9154c107f5
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2022-05-01 09:51:35 +0200

    Create sweep_qlarifais.py

---
+++ mmf/tools/sweeps/sweep_qlarifais.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+import lib as sweep
+from lib import hyperparam
+
+
+def get_grid(args):
+    # For list of args, run `python tools/sweeps/{script_name}.py --help`.
+
+    return [
+        hyperparam("run_type", "train_val"),
+        hyperparam(
+            "training.batch_size", [512, 256], save_dir_key=lambda val: f"bs{val}"
+        ),
+        hyperparam("training.seed", 1, save_dir_key=lambda val: f"s{val}"),
+        hyperparam(
+            "optimizer.params.lr", [5e-5, 1e-5], save_dir_key=lambda val: f"lr{val}"
+        ),
+    ]
+
+
+def postprocess_hyperparams(args, config):
+    """Postprocess a given hyperparameter configuration."""
+    pass
+
+
+if __name__ == "__main__":
+    sweep.main(get_grid, postprocess_hyperparams)
