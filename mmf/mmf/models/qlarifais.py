fixed the fucking gradient

mmf/mmf/models/qlarifais.py
@@ -119,9 +119,11 @@ class Qlarifais(BaseModel):
         else:
             if self.config.image_encoder.resize == 'average_pooling':
                 # average pooling of K features of size 2048
-                image_features = torch.from_numpy(np.nanmean(torch.nan_to_num(image_features, neginf=np.nan).cpu(), axis=1)).to(get_current_device())
-                # [batch_size, i_dim]
+                image_features = torch.nan_to_num(image_features, nan=0, neginf=0).sum(axis=1) / image_features.shape[1] # [batch_size, i_dim]
 
+                # NOT WORKING!!!
+                # torch.from_numpy(np.nanmean(torch.nan_to_num(image_features, neginf=np.nan).detach().cpu(), axis=1)).to(get_current_device())
+            
         # --- FUSION ---
         # type of fusion based on inputs
         if self.config.graph_encoder.use:

mmf/mmf/utils/vocab.py
@@ -461,7 +461,7 @@ class EmbeddedVocab:
         if os.path.exists(saved_dir):
             # load file
             print('Embedded answer vocabulary file found and is loaded')
-            self.embedded_answer_vocab = torch.load(saved_dir)
+            self.embedded_answer_vocab = torch.load(saved_dir, map_location=torch.device('cpu'))
 
         # if desired embedded vocabulary does not exist
         else: