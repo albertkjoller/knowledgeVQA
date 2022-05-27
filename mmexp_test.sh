0503aab827683bc5e92b13f0723e7cc827425389
albertkjoller <albert_k_jacobsen@hotmail.com>
2022-05-27 20:56:04 +0200

    hallelujah - lets go

--- mmexp_test.sh
+++ mmexp_test.sh
@@ -3,7 +3,7 @@
 #BSUB -o /work3/s194253/results/baseline_ama/mmexp_log.out
 #BSUB -e /work3/s194253/results/baseline_ama/mmexp_log.err
 #BSUB -n 6
-#BSUB -q gpua100
+#BSUB -q gpuv100
 #BSUB -gpu 'num=1:mode=exclusive_process'
 #BSUB -W 2:00
 #BSUB -R 'rusage[mem=5GB]'
@@ -34,7 +34,7 @@ python qlarifais_run_explainer.py --model_dir /work3/s194262/save/models/optimiz
     --protocol_dir /zhome/b8/5/147299/Desktop/explainableVQA/examples/protocol \
     --analysis_type OR VisualNoise TextualNoise \
     --explainability_methods MMGradient \
-    --protocol_name baselineQ1.txt \
+    --protocol_name baselineQ.txt \
     --show_all True \
 
 
